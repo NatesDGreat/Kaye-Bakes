4ee6c5dc02bbf1a24a3f19347b7050ff9b638b7b
Nathan <Martineznathan29@gmail.com>
2022-04-26 04:37:03 +0800

    Main

---
+++ Menu.php
@@ -0,0 +1,14 @@
+<?php
+    include 'header.php';
+?>
+
+<form action="search.php" method="POST">
+    <input type="text" name="search" placeholder="Search">
+    <button type="submit" name="submit-search"></button>
+</form>
+
+
+
+<?php
+    include 'footer.php';
+?>
